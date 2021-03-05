c95ea8914d688d0914fa41adc1a6a565d09668a1
Gwendoline Pedurand <gwendoline.pedurand@gmail.com>
2021-03-05 08:39:49 +0100

    pull request

---
+++ routes/Event.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var uid2 =require("uid2")
+
+var hotelModel = require('../models/hotels')
+var userModel = require('../models/users')
+var eventConfirmationModel = require('../models/eventConfirmation')
+var eventsModel = require('../models/events')
+var orderRestaurationModel = require('../models/ordersRestauration')
+var recommandationsModel = require('../models/recommandations')
+var foodModel = require('../models/food')
+
+
