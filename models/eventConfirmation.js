f797ccdebce878750ed4272900c240993dab97cc
hajarCapsule <h.mounjib@gmail.com>
2021-03-05 14:29:17 +0100

    url file

--- models/eventConfirmation.js
+++ models/eventConfirmation.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var eventConfirmationSchema = mongoose.Schema({
     user :{ type: mongoose.Schema.Types.ObjectId, ref: 'users' },
     event: [{ type: mongoose.Schema.Types.ObjectId, ref: 'events' }] ,
+    isComing: Boolean
 })
 
 module.exports = mongoose.model('eventConfirmations', eventConfirmationSchema)
